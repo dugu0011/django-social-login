6b7b44b0fcd166a8fbe702ed6cf7ad46fde7d651
Dugu (headrun) <durgesh@headrun.com>
2023-06-21 16:57:51 +0530

    added django social login

---
+++ Django-CRM-Project/myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/headrun/Documents/djangologiongoogle/Django-CRM-Project/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
